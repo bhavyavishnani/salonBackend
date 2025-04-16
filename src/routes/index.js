3c78e08290f0a2cdd0f1f8d2e5b9af0971f83abe
bhavyavishnani <76779807+bhavyavishnani@users.noreply.github.com>
2025-04-17 00:35:46 +0530

    test commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,14 +4,7 @@ const connectDB = require('../config/db');
 const signupRouter = require('./signup'); // Signup file import
 
 router.get('/test', async (req, res) => {
-    try {
-        const db = await connectDB();
-        const collection = db.collection('testCollection');
-        const data = await collection.find({}).toArray();
-        res.json(data);
-    } catch (error) {
-        res.status(500).json({ error: 'Kuch problem hai database mein' });
-    }
+    res.send('Muh mein lele mera!!😏🍌');
 });
 
 router.use('/', signupRouter); // This will make it /api/signup
